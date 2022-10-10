45b1b8031ece5d6c56a5a55cc2dc68b31e79f79d
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-10-10 11:05:31 +0300

    Added Associations and confirmed ther are working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    belongs_to :vendor
+    has_many :orders
+    has_many :customers, through: :orders
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,6 @@
 class Vendor < ApplicationRecord
     has_secure_password
 
+    has_many :products
+
 end
