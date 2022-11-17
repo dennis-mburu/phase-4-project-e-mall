8047c3f0c0d5b6f1993874c80a22ae94639289a9
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-11-17 10:01:17 +0300

    Implemented grid for all products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import Grid from "@mui/material/Grid";
+
+
 function Products() {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
@@ -16,31 +19,42 @@ function Products() {
       <h1 className="text-center p-3 text-white text-xl font-black">
         ALL AVAILABLE PRODUCTS
       </h1>
-      <div className="flex flex-wrap gap-8 justify-evenly  p-4 font-bold">
+      <div className="mx-8 pb-8" >
+      <Grid container spacing={5} >
+        
+        {/* <div className="flex flex-wrap gap-8 justify-evenly  p-4 font-bold"> */}
         {products.map((product) => (
-          <div className="max-w-sm rounded-xl overflow-hidden shadow-2xl bg-[#1F2937]" key={product.id}>
-            <img
-              className="w-full h-80 object-cover"
-              src={product.image_url}
-              alt="Product"
-            ></img>
-            <div className="px-6 py-4">
-              <div className=" text-xl mb-2 text-center">{product.title}</div>
-              <div className="w-9/12 mx-auto">
-                <p className="text-center ">Ksh. {product.price}</p>
-              </div>
-              <div className="flex justify-center">
-                <button
-                  className=" bg-purple-900 hover:bg-purple-600 mt-5  py-2 px-4 border border-purple-300 hover:border-transparent rounded"
-                  onClick={() => navigate(`/view_product/${product.id}`)}
-                >
-                  Explore Product
-                </button>
+          <Grid item xs={12} sm={6}  md={4} lg={3}>
+            <div
+              className=" rounded-xl overflow-hidden shadow-2xl bg-[#1F2937]"
+              key={product.id}
+            >
+              <img
+                className="w-full h-80 object-cover"
+                src={product.image_url}
+                alt="Product"
+              ></img>
+              <div className="px-6 py-4">
+                <div className=" text-xl mb-2 text-center">{product.title}</div>
+                <div className="w-9/12 mx-auto">
+                  <p className="text-center ">Ksh. {product.price}</p>
+                </div>
+                <div className="flex justify-center">
+                  <button
+                    className=" bg-purple-900 hover:bg-purple-600 mt-5  py-2 px-4 border border-purple-300 hover:border-transparent rounded"
+                    onClick={() => navigate(`/view_product/${product.id}`)}
+                  >
+                    Explore Product
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          </Grid>
         ))}
+        {/* </div> */}
+      </Grid>
       </div>
+
     </>
   );
 }
